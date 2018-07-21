3d2b2772486067e9573cfa0951567e5483d0c50c
niki1980 <38739103+niki1980@users.noreply.github.com>
2018-07-21 15:30:56 -0700

    Term Project
    Beginning pages

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    console.log(document.getElementById ("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+

9e098d4b4a96ecf498a4442ba0782451d762ee4b
Niki Johnson <niki.johnson@cox.net>
2018-07-26 20:24:03 -0700

    term-project
    Adding button reference to registration page and adding serivce pricing

--- scripts/jQuery.js
+++ scripts/jQuery.js
@@ -1,3 +1,18 @@
  $(".accordion").on("click", ".accordion-header", function() {
     $(this).toggleClass("active").next().slideToggle();
  });
+
+
+$(function() {
+   var servicepricing = [];
+
+   $.getJSON('servicepricing.json', function(data) {
+       $.each(data.pricing, function(i, f) {
+          var tblRow = "<tr>" + "<td>" + f.service + "</td>" +
+           "<td>" + f.cost + "</td>" + "<td>"
+           $(tblRow).appendTo("#userdata tbody");
+     });
+
+   });
+
+});

bab25dcdca1854542b249f781552577024f5380f
Niki Johnson <niki.johnson@cox.net>
2018-07-26 20:48:30 -0700

    term project
    playing with service page.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,5 +5,3 @@ function toggleMenu() {
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
